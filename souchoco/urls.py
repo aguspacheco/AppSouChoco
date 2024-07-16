3bbb269b92bba309609fa50b9d5197cded281fa0
Agustin <agus_ph_2@outlook.com>
2024-07-16 20:51:25 -0300

    configurando

--- souchoco/urls.py
+++ souchoco/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from page import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
